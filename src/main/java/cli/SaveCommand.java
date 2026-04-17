9d35439e29e044c16a87c8ecc86545115dbf2ffc
yaghtk-cmyk <yaghtk@gmail.com>
2026-04-17 21:52:36 +0300

    (3 этап)добавление класса SaveCommand

--- src/main/java/cli/SaveCommand.java
+++ src/main/java/cli/SaveCommand.java
@@ -1,4 +1,8 @@
 package cli;
 
-public class SaveCommand {
+public class SaveCommand extends Command {
+
+    public SaveCommand() {
+        super("save", "Сохранить коллекцию в XML файл. Использование: save <имя_файла.xml>");
+    }
 }
