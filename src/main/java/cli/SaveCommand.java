4a77d9cf9fd95faec3ddeca8c70dbf7fda40d357
yaghtk-cmyk <yaghtk@gmail.com>
2026-04-17 22:01:30 +0300

    (3 этап)правка класса SaveCommand

--- src/main/java/cli/SaveCommand.java
+++ src/main/java/cli/SaveCommand.java
@@ -5,4 +5,23 @@ public class SaveCommand extends Command {
     public SaveCommand() {
         super("save", "Сохранить коллекцию в XML файл. Использование: save <имя_файла.xml>");
     }
+
+    @Override
+    public void execute(String[] args, CommandContext context) {
+        if (args.length == 0) {
+            System.out.println("Ошибка: Вы не указали путь к файлу!");
+            System.out.println("Пример правильного ввода: save data.xml");
+            return;
+        }
+
+        String path = args[0];
+
+        try {
+            // ИСПОЛЬЗУЕМ unitManager() ТАК КАК ЭТО RECORD
+            context.unitManager().saveToFile(path);
+
+        } catch (Exception e) {
+            System.out.println("Критическая ошибка при сохранении: " + e.getMessage());
+        }
+    }
 }
