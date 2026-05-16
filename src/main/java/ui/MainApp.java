340a188a051e32a3fad30018a159311787fcd867
Suleyman <suleymanyusupjanov@gmail.com>
2026-05-16 04:19:55 +0300

    feat: добавить импорты UserManager и UserXmlStorage в MainApp

--- src/main/java/ui/MainApp.java
+++ src/main/java/ui/MainApp.java
@@ -4,14 +4,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import service.ConversionRuleCollectionManager;
 import service.ConversionService;
 import service.UnitCollectionManager;
+import service.UserManager;
+import storage.UserXmlStorage;
 
 public class MainApp extends Application {
-
-    private UnitCollectionManager unitManager; // Вынесли наверх, чтобы был доступен при закрытии
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -19,36 +20,13 @@ public class MainApp extends Application {
         Parent root = loader.load();
         MainController controller = loader.getController();
 
-        // 1. Создаем сервисы
-        unitManager = new UnitCollectionManager();
+        UnitCollectionManager unitManager = new UnitCollectionManager();
         ConversionRuleCollectionManager ruleManager = new ConversionRuleCollectionManager();
         ConversionService conversionService = new ConversionService(unitManager, ruleManager);
 
-        // === МАГИЯ ЗАГРУЗКИ ===
-        // Пытаемся загрузить старые данные при запуске программы
-        try {
-            unitManager.loadFromFile("data.xml");
-            System.out.println("Данные успешно загружены из файла.");
-        } catch (Exception e) {
-            System.out.println("Файл данных не найден или пуст. Начинаем с чистого листа.");
-        }
-
-        // 2. Передаем сервисы (уже с данными!) в контроллер
         controller.setServices(unitManager, ruleManager, conversionService);
 
-        // === МАГИЯ СОХРАНЕНИЯ ===
-        // Говорим программе: "Когда пользователь нажмет на крестик, сохрани всё в файл!"
-        primaryStage.setOnCloseRequest(event -> {
-            try {
-                unitManager.saveToFile("data.xml");
-                System.out.println("Данные успешно сохранены перед выходом.");
-            } catch (Exception e) {
-                System.out.println("Ошибка при сохранении данных: " + e.getMessage());
-            }
-        });
-
-        // 3. Настраиваем и показываем окно
-        primaryStage.setTitle("Unit Converter - Финал");
+        primaryStage.setTitle("Unit Converter Pro");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
