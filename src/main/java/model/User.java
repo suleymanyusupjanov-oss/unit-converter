a1762f0500545c94185ef031167a3c5734e9c0d9
Suleyman <suleymanyusupjanov@gmail.com>
2026-05-16 03:59:57 +0300

    feat: добавить конструкторы в User

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -8,4 +8,13 @@ public final class User {
     private String login;
     private String passwordHash;
     private Instant createdAt;
+
+    public User() {}
+
+    public User(long id, String login, String passwordHash, Instant createdAt) {
+        this.id = id;
+        this.login = login;
+        this.passwordHash = passwordHash;
+        this.createdAt = createdAt;
+    }
 }
